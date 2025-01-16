c23a094bda5a6b15c54af357a592e7c14c999347
valadao-davi <davi.raichu@gmail.com>
2025-01-16 19:44:29 -0300

    feat: sellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 
 		System.out.println(obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.CreateSellerDao();
 	}
 
 }
