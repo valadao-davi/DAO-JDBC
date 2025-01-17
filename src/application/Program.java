1d46929409a4a4a349d203f270a31b54973e0530
valadao-davi <davi.raichu@gmail.com>
2025-01-16 23:31:40 -0300

    feat: findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,10 +15,18 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.CreateSellerDao(); // Essa interface vai receber os métodos do JDBC
-		System.out.println("=== FIND USER BY ID AND RETURN IN BASE OF DEPARTMENT ID ===");
+		System.out.println("=== FIND SELLER BY ID AND RETURN IN BASE OF DEPARTMENT ID ===");
 		int id = sc.nextInt();
 		
 		Seller seller  = sellerDao.findById(id);
+		
+		System.out.println("=== FIND SELLER BY DEPARTMENT ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller sel : list) {
+			System.out.println(sel);
+		}
+
 		sc.close();
 		
 		System.out.println(seller);
