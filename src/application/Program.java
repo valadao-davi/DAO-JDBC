b8652e3c96d5b1446f02b05de1963dc3658ceb2d
valadao-davi <davi.raichu@gmail.com>
2025-01-16 23:38:13 -0300

    feat: findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,21 +13,25 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.CreateSellerDao(); // Essa interface vai receber os métodos do JDBC
 		System.out.println("=== FIND SELLER BY ID AND RETURN IN BASE OF DEPARTMENT ID ===");
-		int id = sc.nextInt();
 		
-		Seller seller  = sellerDao.findById(id);
+		Seller seller  = sellerDao.findById(3);
+		System.out.println(seller);
 		
-		System.out.println("=== FIND SELLER BY DEPARTMENT ===");
+		System.out.println("\n === FIND SELLER BY DEPARTMENT ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		for(Seller sel : list) {
+		for(Seller sel : list) { 
+			System.out.println(sel);
+		}
+		
+		System.out.println("\n=== FIND SELLER ALL ===");
+		list = sellerDao.findAll();
+		for(Seller sel : list) { 
 			System.out.println(sel);
 		}
 
-		sc.close();
 		
 		System.out.println(seller);
 		
