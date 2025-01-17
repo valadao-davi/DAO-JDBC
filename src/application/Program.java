6ae50cc29141da566bab93e9935a52c382e65926
valadao-davi <davi.raichu@gmail.com>
2025-01-17 00:16:39 -0300

    feat: delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.CreateSellerDao(); // Essa interface vai receber os métodos do JDBC
 		System.out.println("\n=== FIND SELLER BY ID AND RETURN IN BASE OF DEPARTMENT ID ===");
@@ -41,6 +43,13 @@ public class Program {
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
+		
+		System.out.println("\n=== DELETE SELLER ===");
+		System.out.println("Enter id for delete Seller");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
+		sc.close();
 	}
 
 }
